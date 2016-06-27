e7b0bbbfe7ea7373314a8717d640acf40a2b203a
Harsha <lharsha4@gmail.com>
2016-06-27 20:04:55 +0530

    Converted to folder

---
+++ Angular_KickStart_Basic/README.md
@@ -0,0 +1,2 @@
+# Angular2-Basics
+Basic Concept of How to Start Angular 2
---
+++ Angular_KickStart_Basic/gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var typescript = require('gulp-typescript');//This is to convert typescript to javascript
+var sourcemaps = require('gulp-sourcemaps');//This is to generate sourcemap files (Useful for debugging in typescript)
+var tsProject = typescript.createProject('tsconfig.json');
+
+var appDev = 'dev/';
+var appProd = 'app/';
+var libPath = 'lib/'
+
+gulp.task('build-ts', function() {
+
+    return gulp.src(appDev+'**/*.ts')
+        .pipe(sourcemaps.init())
+        .pipe(typescript(tsProject))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(appProd));
+});
+
+gulp.task('watch', function() {
+
+    gulp.watch(appDev+'**/*.ts',['build-ts']);
+});
+
+gulp.task('cp-vendor-scripts', function() {
+
+    return gulp.src([
+        'node_modules/es6-shim/es6-shim.min.js',
+        'node_modules/systemjs/dist/system-polyfills.js',
+        'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
+        'node_modules/angular2/bundles/angular2-polyfills.js',
+        'node_modules/systemjs/dist/system.src.js',
+        'node_modules/rxjs/bundles/Rx.js',
+        'node_modules/angular2/bundles/angular2.js'
+    ]).pipe(gulp.dest(libPath));
+})
+
+gulp.task('default', ['cp-vendor-scripts','watch', 'build-ts']);
